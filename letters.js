ce32087dad8d8083a2ce819fc72e995704d4161a
Phillip <phv9495@saintjoe.edu>
2015-01-26 13:39:09 -0500

    I just realized that I hadn't uploaded the file after having finished the program. Here is the finished version.

--- letters.js
+++ letters.js
@@ -1,6 +1,17 @@
 // Phil Hofer
 // 1/19/15
 // letters.js
+// This program creates an object that stores individual letters 
+// in an array and has a function for displaying the letters as a single word.
 
 //This array is what will be used to store the individual characters 
-var letterArray = [];
+var letterArray = ["H", "E", "L", "L", "O"];
+
+//This function joins all of the individual characters together and prints
+//them to the console.
+function singleWord() {
+  console.log(letterArray.join(""));
+}
+
+//This calls the function singleWord().
+singleWord();
